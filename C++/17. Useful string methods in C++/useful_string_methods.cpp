237942e1c43687afcc5f78557f3afa618d746f96
Henry-Huynh-pap <conghuy0511@gmail.com>
2026-08-07 10:04:30 +0700

    useful string methods

---
+++ C++/17. Useful string methods in C++/useful_string_methods.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+int main (){
+    
+    std::string name;
+    
+    std::cout << "Enter your name: ";
+    std::getline (std::cin, name);
+
+    /*
+    if(name.length() >12 ) {
+        std::cout << "Your name can't be over 12 characters";
+    }
+    else {
+        std::cout << "Welcome " << name;
+    }
+    */
+
+    /*
+    if(name.empty()){
+        std::cout << "Your name is empty";
+    }
+    else{
+        std::cout <<"Welcome " << name;
+    }
+    */
+    
+    /*
+    name.clear();
+    std::cout << "Welcome " << name;
+    */
+
+    /*
+    name.append("@gmail.com");
+
+    std::cout << "Your username is now " << name;
+    */
+
+    /*
+    std::cout << name.at(0);
+    */
+
+    /*
+    name.insert(1, "@");
+
+    std::cout << name;
+    */
+
+    /*
+    std::cout << name.find(" ");
+    */
+
+    name.erase(0, 5);
+    std::cout << name;
+
+    return 0;
+}
