0407b0c26be014f410e096a6114bc5fa43684bac
Henry-Huynh-pap <conghuy0511@gmail.com>
2026-08-17 10:38:47 +0700

    nested_loops.cpp

---
+++ C++/22. Nested loops ➿/dia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main() {
+
+    int rows;
+    int colums;
+    char symbol;
+
+    std::cout << "Enter your rows: ";
+    std::cin >> rows;
+    
+    std::cout << "Enter your colums: ";
+    std::cin >> colums;
+
+    std::cout << "Enter your symbol: ";
+    std::cin >> symbol;
+
+    for(int i =1; i <= rows; i++){
+        for(int j=1; j <=colums; j++){
+            std::cout << symbol;
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
