fb5cf336aea7e791fb5444933fbc235e32146bb2
Henry-Huynh-pap <conghuy0511@gmail.com>
2026-08-20 09:47:13 +0700

    dia

---
+++ C++/25.  Number guessing game ↕️/dia.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+int main () {
+
+    int num;
+    int guess;
+    int tries = 0;
+
+    srand(time(0));
+    num = rand() % 100 + 1;
+
+    std::cout << "******NUMBER GUESSING GAME******\n";
+
+    do {
+        std::cout << "Please enter your num guessing: ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num){
+            std::cout << "Too high\n";
+        }
+        else if (guess < num){
+            std::cout << "Too low\n";
+        }
+        else{
+            std::cout << "The correct of tries is: " << tries << '\n';
+        }
+    }while (guess != num);
+
+    std::cout << "***************";
+
+    return 0;
+}
